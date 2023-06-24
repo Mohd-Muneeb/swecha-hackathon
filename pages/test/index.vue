6835ef644e723b5b298c37b631427c1f652466b3
Harish Pillarisetti <44186210+hpsetti@users.noreply.github.com>
2023-06-24 13:40:03 +0530

    Initial commit

---
+++ pages/test/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="">
+    <h1 class="text-subtitle-1">hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test-index",
+  mounted() {
+    // this.locatorButtonPressed();
+  },
+  methods: {
+    locatorButtonPressed() {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position.coords.latitude);
+          console.log(position.coords.longitude);
+        },
+        (error) => {
+          console.log(error.message);
+        }
+      );
+    },
+  },
+};
+</script>
+
+<style></style>
